25d4019db7713139be6502ede3cafd04da658143
matiasgandolfi <101219833+matiasgandolfi@users.noreply.github.com>
2023-10-30 01:29:43 -0300

    First

---
+++ Compartir/frmCompartir.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCompartir
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRevertir = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtRuta = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnRevertir
+        '
+        Me.btnRevertir.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnRevertir.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue
+        Me.btnRevertir.FlatAppearance.BorderSize = 0
+        Me.btnRevertir.FlatAppearance.CheckedBackColor = System.Drawing.Color.RoyalBlue
+        Me.btnRevertir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnRevertir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.btnRevertir.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnRevertir.Location = New System.Drawing.Point(327, 331)
+        Me.btnRevertir.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnRevertir.Name = "btnRevertir"
+        Me.btnRevertir.Size = New System.Drawing.Size(165, 60)
+        Me.btnRevertir.TabIndex = 18
+        Me.btnRevertir.Text = "&Dejar de Compartir"
+        Me.btnRevertir.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label4.Location = New System.Drawing.Point(138, 271)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 29)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "Ruta:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label3.Location = New System.Drawing.Point(131, 223)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(421, 29)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Indique la ruta de se carpeta ""Gestion"""
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.LightGray
+        Me.Label2.Location = New System.Drawing.Point(118, 60)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(549, 63)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "MEGA-INSTALADOR"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label1.Location = New System.Drawing.Point(129, 123)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(554, 31)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Compartir carpeta Gestion Megasoft por Red"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.RoyalBlue
+        Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue
+        Me.Button3.FlatAppearance.BorderSize = 0
+        Me.Button3.FlatAppearance.CheckedBackColor = System.Drawing.Color.RoyalBlue
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button3.Location = New System.Drawing.Point(518, 331)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(165, 60)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "&Cancelar"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.RoyalBlue
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.CheckedBackColor = System.Drawing.Color.RoyalBlue
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Location = New System.Drawing.Point(136, 331)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(165, 60)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "&Compartir por Red"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'txtRuta
+        '
+        Me.txtRuta.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRuta.Location = New System.Drawing.Point(213, 271)
+        Me.txtRuta.Name = "txtRuta"
+        Me.txtRuta.Size = New System.Drawing.Size(262, 30)
+        Me.txtRuta.TabIndex = 11
+        Me.txtRuta.Text = "C:\Ejemplo"
+        '
+        'frmCompartir
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnRevertir)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtRuta)
+        Me.Name = "frmCompartir"
+        Me.Text = "frmCompartir"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnRevertir As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtRuta As TextBox
+End Class
---
+++ Compartir/frmCompartir.vb
@@ -0,0 +1,282 @@
+﻿Imports System.Management
+Imports Microsoft.Win32
+Imports System.IO
+Imports System.Security.AccessControl
+
+Public Class frmCompartir
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ruta As String = txtRuta.Text
+        Dim result As String = ""
+
+        result &= CompartirCarpeta(ruta)
+        result &= ConfigurarPermisosCompartidos(ruta)
+        result &= ConfigurarPermisosSeguridad(ruta)
+        DarControlTotalATodos(ruta)
+        DesactivarSolicitudesCredenciales()
+        DesactivarUsoCompartidoConProteccionPorContrasena()
+
+        MsgBox("Se ha compartido la carpeta correctamente")
+
+
+        MsgBox("Cambios realizados:" & vbCrLf & result)
+    End Sub
+
+
+    Private Function CompartirCarpeta(ruta As String) As String
+        Return RunProcessAndGetOutput("net", "share " & Path.GetFileName(ruta) & "=" & ruta)
+    End Function
+
+    Private Function ConfigurarPermisosCompartidos(ruta As String) As String
+        Return RunProcessAndGetOutput("icacls", ruta & " /grant Todos:(OI)(CI)M")
+    End Function
+
+    Private Function ConfigurarPermisosSeguridad(ruta As String) As String
+        Return RunProcessAndGetOutput("icacls", ruta & " /grant *S-1-1-0:(OI)(CI)F")
+    End Function
+
+
+    Private Sub DarControlTotalATodos(ruta As String)
+        Try
+            Dim carpetaInfo As New DirectoryInfo(ruta)
+            Dim acl As DirectorySecurity = carpetaInfo.GetAccessControl()
+            Dim rule As New FileSystemAccessRule("Todos", FileSystemRights.FullControl, InheritanceFlags.ContainerInherit Or InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow)
+            acl.AddAccessRule(rule)
+            carpetaInfo.SetAccessControl(acl)
+        Catch ex As Exception
+            MsgBox("Error al modificar los permisos: " & ex.Message)
+        End Try
+    End Sub
+
+
+    ' Desactivar el uso compartido con protección por contraseña
+    Public Sub DesactivarSolicitudesCredenciales()
+        Try
+            Dim scope As New ManagementScope("\\.\root\cimv2")
+            Dim query As New ObjectQuery("SELECT * FROM Win32_Service WHERE Name='LanmanServer'")
+
+            Using searcher As New ManagementObjectSearcher(scope, query)
+                Dim services As ManagementObjectCollection = searcher.Get()
+
+                For Each service As ManagementObject In services
+                    Dim args() As Object = {Nothing}
+                    service.InvokeMethod("ChangeStartMode", args)
+
+                    Dim inParams As ManagementBaseObject = service.GetMethodParameters("ChangeStartMode")
+                    inParams("StartMode") = 2 ' Automatic start
+                    service.InvokeMethod("ChangeStartMode", inParams, Nothing)
+                Next
+            End Using
+
+            ' Desactivar el uso compartido con protección por contraseña
+            Dim sharingParametersPath As String = "SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
+            Dim sharingParameters As RegistryKey = Registry.LocalMachine.OpenSubKey(sharingParametersPath, True)
+
+            If sharingParameters IsNot Nothing Then
+                sharingParameters.SetValue("restrictnullsessaccess", 0)
+            Else
+                MsgBox("La clave del Registro 'restrictnullsessaccess' no se encontró en la ubicación especificada.")
+            End If
+        Catch ex As Exception
+            ' Manejo de excepciones: muestra un mensaje de error en caso de que ocurra una excepción.
+            MsgBox("Error al desactivar las solicitudes de credenciales: " & ex.Message)
+        End Try
+    End Sub
+
+
+    'Public Sub DesactivarUsoCompartidoConProteccionPorContrasena3()
+    '    Try
+    '        Dim scope As New ManagementScope("\\.\root\cimv2")
+    '        Dim query As New ObjectQuery("SELECT * FROM Win32_ShareSecuritySetting")
+
+    '        Using searcher As New ManagementObjectSearcher(scope, query)
+    '            Dim shares As ManagementObjectCollection = searcher.Get()
+
+    '            For Each share As ManagementObject In shares
+    '                If share("SecurityDescriptor") IsNot Nothing Then
+    '                    Dim securityDescriptor As ManagementBaseObject = DirectCast(share("SecurityDescriptor"), ManagementBaseObject)
+    '                    Dim controlFlags As Integer = CInt(securityDescriptor("ControlFlags"))
+
+    '                    ' Desactivar la protección por contraseña (quitar el bit 0x02)
+    '                    controlFlags = controlFlags And Not &H2
+    '                    securityDescriptor("ControlFlags") = controlFlags
+    '                    'securityDescriptor.Put()
+    '                End If
+    '            Next
+    '        End Using
+
+    '        MsgBox("El uso compartido con protección por contraseña se ha desactivado correctamente.")
+
+    '    Catch ex As Exception
+    '        ' Manejo de excepciones: muestra un mensaje de error en caso de que ocurra una excepción.
+    '        MsgBox("Error al desactivar el uso compartido con protección por contraseña: " & ex.Message)
+    '    End Try
+    'End Sub
+
+
+    Public Sub VerificarExistenciaClaseWMI()
+        Try
+            Dim scope As New ManagementScope("\\.\root\cimv2")
+            Dim query As New ObjectQuery("SELECT * FROM meta_class")
+
+            Using searcher As New ManagementObjectSearcher(scope, query)
+                Dim classes As ManagementObjectCollection = searcher.Get()
+
+                Dim claseBuscada As String = "Win32_ShareSecuritySetting"
+                Dim claseEncontrada As Boolean = False
+
+                For Each classObject As ManagementObject In classes
+                    Dim className As String = CStr(classObject("Name"))
+
+                    If className = claseBuscada Then
+                        claseEncontrada = True
+                        Exit For
+                    End If
+                Next
+
+                If claseEncontrada Then
+                    MsgBox("La clase 'Win32_ShareSecuritySetting' está disponible en el sistema.")
+                Else
+                    MsgBox("La clase 'Win32_ShareSecuritySetting' no se encontró en el sistema.")
+                End If
+            End Using
+
+        Catch ex As Exception
+            ' Manejo de excepciones: muestra un mensaje de error en caso de que ocurra una excepción.
+            MsgBox("Error al verificar la existencia de la clase: " & ex.Message)
+        End Try
+    End Sub
+
+
+    '***********************************************************************
+    Public Sub DesactivarUsoCompartidoConProteccionPorContrasena()
+        Try
+            VerificarExistenciaClaseWMI()
+            Dim scope As New ManagementScope("\\.\root\cimv2")
+            Dim query As New ObjectQuery("SELECT * FROM Win32_ShareSecuritySetting")
+
+            Using searcher As New ManagementObjectSearcher(scope, query)
+                Dim shares As ManagementObjectCollection = searcher.Get()
+
+                For Each share As ManagementObject In shares
+                    If share("SecurityDescriptor") IsNot Nothing Then
+                        Dim securityDescriptor As ManagementObject = DirectCast(share("SecurityDescriptor"), ManagementObject)
+                        Dim controlFlags As Integer = CInt(securityDescriptor("ControlFlags"))
+
+                        ' Desactivar la protección por contraseña (quitar el bit 0x02)
+                        controlFlags = controlFlags And Not &H2
+                        securityDescriptor("ControlFlags") = controlFlags
+                        securityDescriptor.Put()
+                    End If
+                Next
+            End Using
+
+            MsgBox("El uso compartido con protección por contraseña se ha desactivado correctamente.")
+
+        Catch ex As Exception
+            ' Manejo de excepciones: muestra un mensaje de error en caso de que ocurra una excepción.
+            MsgBox("Error al desactivar el uso compartido con protección por contraseña: " & ex.Message)
+        End Try
+    End Sub
+
+
+
+
+    Public Sub RevertirCambios(ruta As String)
+        Try
+            ' Eliminar el recurso compartido de la carpeta
+            Dim eliminarCompartido As String = "net share " & Path.GetFileName(ruta) & " /delete"
+            RunProcessAndGetOutput("cmd", "/c " & eliminarCompartido)
+
+            ' Restablecer permisos de compartición
+            Dim restablecerPermisosCompartidos As String = "icacls " & ruta & " /remove:d Todos"
+            RunProcessAndGetOutput("cmd", "/c " & restablecerPermisosCompartidos)
+
+            ' Restablecer permisos de seguridad
+            Dim restablecerPermisosSeguridad As String = "icacls " & ruta & " /remove:g *S-1-1-0"
+            RunProcessAndGetOutput("cmd", "/c " & restablecerPermisosSeguridad)
+
+            ' Restablecer permisos de control total
+            Dim carpetaInfo As New DirectoryInfo(ruta)
+            Dim acl As DirectorySecurity = carpetaInfo.GetAccessControl()
+            Dim rule As New FileSystemAccessRule("Todos", FileSystemRights.FullControl, AccessControlType.Deny)
+            acl.RemoveAccessRule(rule)
+            carpetaInfo.SetAccessControl(acl)
+
+            ' Restablecer el uso compartido con protección por contraseña
+            Dim sharingParametersPath As String = "SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
+            Dim sharingParameters As RegistryKey = Registry.LocalMachine.OpenSubKey(sharingParametersPath, True)
+
+            If sharingParameters IsNot Nothing Then
+                sharingParameters.DeleteValue("restrictnullsessaccess", False)
+            End If
+
+            MsgBox("Cambios revertidos con éxito.")
+        Catch ex As Exception
+            ' Manejo de excepciones: muestra un mensaje de error en caso de que ocurra una excepción.
+            MsgBox("Error al revertir los cambios: " & ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub btnRevertir_Click(sender As Object, e As EventArgs) Handles btnRevertir.Click
+        RevertirCambios(txtRuta.Text)
+
+    End Sub
+
+
+
+    Private Function RunProcessAndGetOutput(filename As String, arguments As String) As String
+        Try
+            Dim proceso As New Process()
+            proceso.StartInfo.FileName = filename
+            proceso.StartInfo.Arguments = arguments
+            proceso.StartInfo.RedirectStandardOutput = True
+            proceso.StartInfo.UseShellExecute = False
+            proceso.StartInfo.CreateNoWindow = True
+            proceso.Start()
+            Dim output As String = proceso.StandardOutput.ReadToEnd()
+            proceso.WaitForExit()
+
+            Return output
+        Catch ex As Exception
+            ' Manejo de excepciones: muestra un mensaje de error en caso de que ocurra una excepción.
+            MsgBox("Error al ejecutar el proceso: " & ex.Message)
+            Return ""
+        End Try
+    End Function
+
+
+    Public Sub DesactivarUsoCompartidoConProteccionPorContrasena2()
+        Try
+            ' Abre la clave del Registro relacionada con la configuración avanzada de uso compartido
+            Dim advancedSharingSettings As RegistryKey = Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Policies\Explorer", True)
+
+            ' Verifica si la clave existe; si no, créala
+            If advancedSharingSettings Is Nothing Then
+                advancedSharingSettings = Registry.CurrentUser.CreateSubKey("Software\Microsoft\Windows\CurrentVersion\Policies\Explorer", RegistryKeyPermissionCheck.ReadWriteSubTree)
+            End If
+
+            ' Establece el valor de restricción de sesión nula (restrictnullsessaccess) en 0 para desactivar el uso compartido con protección por contraseña
+            advancedSharingSettings.SetValue("restrictnullsessaccess", 0, RegistryValueKind.DWord)
+
+            ' Cierra la clave del Registro
+            advancedSharingSettings.Close()
+
+            MsgBox("El uso compartido con protección por contraseña se ha desactivado correctamente.")
+
+        Catch ex As Exception
+            ' Manejo de excepciones: muestra un mensaje de error en caso de que ocurra una excepción.
+            MsgBox("Error al desactivar el uso compartido con protección por contraseña: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmCompartir_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+End Class
